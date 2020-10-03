92f0f8449d3a9fd2afe4efc82270414f98dce263
Shrey2002 <72299720+Shrey2002@users.noreply.github.com>
2020-10-03 16:06:06 +0530

    Create Basic_data.cpp

---
+++ Hackerrank/Introduction/Basic_data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf",&n,&l,&c,&f,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf",n,l,c,f,d);
+    // Complete the code.
+    return 0;
+}
